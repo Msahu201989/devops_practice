3043ab131e7acf52a021516e597debf9ed7aef3f
Mukesh <Stunty@007>
2023-01-28 20:22:41 +0530

    learning devops

--- 03/ec2.tf
+++ 03/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "sample" {
+resource "aws_instance" "mukesh" {
   ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
 #  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
