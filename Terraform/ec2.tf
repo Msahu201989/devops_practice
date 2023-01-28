b966357dd8f30f0ab89cbd0fe620d319a5c97f1c
Mukesh <Stunty@007>
2023-01-28 19:57:35 +0530

    learning devops

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "use-east-1"
 }
 
-resource "aws_instance" "sample" {
+resource "aws_instance" "mukesh" {
   ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
 }
